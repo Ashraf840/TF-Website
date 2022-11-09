552965582e86e98e1798867a2081c6d2a681acc5
tanjim840 <tanjim.techforing@gmail.com>
2022-11-09 18:14:52 +0600

    Updated uwsgi.sh; checked & create maintf_uwsgi.log

--- uwsgi.sh
+++ uwsgi.sh
@@ -7,6 +7,15 @@ echo "Present Dir: $PWD"
 source env/bin/activate
 pip3 install uwsgi
 
+if [ -e /var/lib/jenkins/workspace/MainTF/logs/maintf_uwsgi.log ]
+then
+    echo "maintf_uwsgi.log file exists"
+else
+    echo "maintf_uwsgi.log file doesn't exists"
+    touch logs/maintf_uwsgi.log
+    echo "Created the maintf_uwsgi.log file into path: $PWD/logs/maintf_uwsgi.log"
+fi
+
 sudo systemctl daemon-reload
 # sudo systemctl restart emperor.uwsgi.service
 sudo systemctl status emperor.uwsgi.service
