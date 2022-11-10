9ea74e8e65941f23c24fa78be4784a94d7a8c7f9
tanjim840 <tanjim.techforing@gmail.com>
2022-11-10 11:21:12 +0600

    Added 'djangoblog.techforing.com' in hosts.py

--- MainTechforing/hosts.py
+++ MainTechforing/hosts.py
@@ -7,9 +7,10 @@ host_patterns = patterns(
     host(r'', settings.ROOT_URLCONF, name='main'),
     host(r'academy', 'Academy.urls', name='academy'),
     host(r'mysecurity', 'PersonalSecurity.urls', name='mysecurity'),
-    host(r'', settings.ROOT_URLCONF, name='blog'),
+    # host(r'', settings.ROOT_URLCONF, name='blog'),
     host(r'', settings.ROOT_URLCONF, name='account'),
     host(r'', settings.ROOT_URLCONF, name='api'),
+    host(r'djangoblog', 'Blog.urls', name='blog')
 
 
     # For Development
