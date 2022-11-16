6f8b45c9aa689fbb39c22b278b3dca23114f58b2
tanjim840 <tanjim.techforing@gmail.com>
2022-11-16 15:00:59 +0600

    Un-comment login & singup router

--- Account/urls.py
+++ Account/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from Account import views
 
 urlpatterns = [
-    # path('login/', views.loginView, name='login'),
-    # path('signup/', views.signupView, name='signup'),
+    path('login/', views.loginView, name='login'),
+    path('signup/', views.signupView, name='signup'),
     path('logout/', views.logoutView, name='logout'),
     path('profile/', views.profileView, name='user_profile'),
     path('change_password/', views.passwordChangeView, name='change_password'),
